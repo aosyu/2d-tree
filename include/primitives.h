aa55a812bbbc65d59203d8c0b2eda201f77c086a
Anna Syuris <ann.syuris@gmail.com>
2021-05-18 20:39:04 +0300

    solution added

--- include/primitives.h
+++ include/primitives.h
@@ -1,37 +1,87 @@
 #pragma once
 
+#include <fstream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <optional>
+#include <set>
+#include <valarray>
+#include <vector>
+#include <functional>
+#include <iostream>
+
 class Point
 {
 public:
-    Point(double x, double y);
+    Point()
+        : m_x(0)
+        , m_y(0)
+    {
+    }
 
-    double x() const;
-    double y() const;
-    double distance(const Point &) const;
+    Point(double x, double y)
+        : m_x(x)
+        , m_y(y)
+    {
+    }
 
-    bool operator<(const Point &) const;
-    bool operator>(const Point &) const;
-    bool operator<=(const Point &) const;
-    bool operator>=(const Point &) const;
-    bool operator==(const Point &) const;
-    bool operator!=(const Point &) const;
+    double x() const { return m_x; }
+    double y() const { return m_y; }
+    double distance(const Point & p) const
+    {
+        const double x = p.x() - m_x;
+        const double y = p.y() - m_y;
+        return std::sqrt(x * x + y * y);
+    }
 
-    friend std::ostream & operator<<(std::ostream &, const Point &);
+    bool operator<(const Point & p) const { return (m_x == p.x() && m_y < p.y()) || (m_x < p.x()); }
+    bool operator>(const Point & p) const { return (m_x == p.x() && m_y > p.y()) || (m_x > p.x()); }
+    bool operator<=(const Point & p) const { return (m_x == p.x() && m_y <= p.y()) || (m_x < p.x()); }
+    bool operator>=(const Point & p) const { return (m_x == p.x() && m_y >= p.y()) || (m_x > p.x()); }
+    bool operator==(const Point & p) const { return m_x == p.x() && m_y == p.y(); }
+    bool operator!=(const Point & p) const { return !(m_x == p.x() && m_y == p.y()); }
+
+    friend std::ostream & operator<<(std::ostream & out, const Point & p)
+    {
+        out << '(' << p.x() << ", " << p.y() << ')';
+        return out;
+    }
+
+private:
+    double m_x;
+    double m_y;
 };
 
 class Rect
 {
 public:
-    Rect(const Point & left_bottom, const Point & right_top);
+    Rect(const Point & left_bottom, const Point & right_top)
+        : m_left_bottom(left_bottom)
+        , m_right_top(right_top)
+    {
+    }
 
-    double xmin() const;
-    double ymin() const;
-    double xmax() const;
-    double ymax() const;
-    double distance(const Point & p) const;
+    double xmin() const { return m_left_bottom.x(); }
+    double ymin() const { return m_left_bottom.y(); }
+    double xmax() const { return m_right_top.x(); }
+    double ymax() const { return m_right_top.y(); }
+    double distance(const Point & p) const
+    {
+        const double x = std::max({xmin() - p.x(), p.x() - xmax(), 0.0});
+        const double y = std::max({ymin() - p.y(), p.y() - ymax(), 0.0});
+        return sqrt(x * x + y * y);
+    }
 
-    bool contains(const Point & p) const;
-    bool intersects(const Rect &) const;
+    bool contains(const Point & p) const { return p.x() > xmin() && p.y() > ymin() && p.x() < xmax() && p.y() < ymax(); }
+    bool intersects(const Rect & rect) const
+    {
+        return !(m_right_top < rect.m_left_bottom) && !(rect.m_right_top < m_left_bottom);
+    }
+
+private:
+    Point m_left_bottom;
+    Point m_right_top;
 };
 
 namespace rbtree {
@@ -39,45 +89,160 @@ namespace rbtree {
 class PointSet
 {
 public:
-    class iterator
+    using iterator = std::set<Point>::iterator;
+    PointSet(const std::string & filename = {})
     {
-        using iterator_category = std::forward_iterator_tag;
-        // To do
-    };
+        std::ifstream in(filename);
+        if (in.is_open()) {
+            double x, y;
+            while (in >> x >> y) {
+                m_tree.emplace(x, y);
+            }
+        }
+        in.close();
+    }
+    bool empty() const { return m_tree.empty(); }
+    std::size_t size() const { return m_tree.size(); }
+    void put(const Point & p) { m_tree.insert(p); }
+    bool contains(const Point & p) const { return m_tree.end() != m_tree.find(p); }
+    std::pair<iterator, iterator> range(const Rect & r) const
+    {
+        std::set<Point> * m_range_result = new std::set<Point>();
+        range_answer.push_back(std::shared_ptr<std::set<Point>>(m_range_result));
+        for (const auto it : m_tree) {
+            if (r.contains(it)) {
+                m_range_result->insert(it);
+            }
+        }
+        return {m_range_result->begin(), m_range_result->end()};
+    }
 
-    PointSet();
+    iterator begin() const { return m_tree.begin(); }
+    iterator end() const { return m_tree.end(); }
 
-    bool empty() const;
-    std::size_t size() const;
-    void put(const Point &);
-    bool contains(const Point &) const;
+    std::optional<Point> nearest(const Point & p) const
+    {
+        return (*std::min_element(begin(), end(), [&p](const Point & a, const Point & b) {
+            return a.distance(p) < b.distance(p);
+        }));
+    }
+    std::pair<iterator, iterator> nearest(const Point & p, std::size_t k) const
+    {
+        std::set<Point> * m_nearest_result = new std::set<Point>();
+        nearest_answer.push_back(std::shared_ptr<std::set<Point>>(m_nearest_result));
+        std::map<double, Point> map;
+        for (const auto it : m_tree) {
+            map.insert({p.distance(it), it});
+        }
+        auto it_ans = map.begin();
+        for (std::size_t i = 0; i < k; i++) {
+            if (it_ans == map.end()) {
+                break;
+            }
+            m_nearest_result->emplace((*it_ans).second);
+            it_ans++;
+        }
+        return {m_nearest_result->begin(), m_nearest_result->end()};
+    }
 
-    // second iterator points to an element out of range
-    std::pair<iterator, iterator> range(const Rect &) const;
-    iterator begin() const;
-    iterator end() const;
+    friend std::ostream & operator<<(std::ostream & out, const PointSet & ps)
+    {
+        for (const auto & it : ps) {
+            out << it;
+        }
+        return out;
+    }
 
-    std::optional<Point> nearest(const Point &) const;
-    // second iterator points to an element out of range
-    std::pair<iterator, iterator> nearest(const Point & p, std::size_t k) const;
-
-    friend std::ostream & operator<<(std::ostream &, const PointSet &);
+private:
+    mutable std::vector<std::shared_ptr<std::set<Point>>> range_answer;
+    mutable std::vector<std::shared_ptr<std::set<Point>>> nearest_answer;
+    std::set<Point> m_tree;
 };
 
 } // namespace rbtree
 
 namespace kdtree {
+//using PointSet = rbtree::PointSet;
 
 class PointSet
 {
 public:
-    class iterator
+    struct Node
     {
-        using iterator_category = std::forward_iterator_tag;
-        // To do
+        Node(const Point & p, bool v, const Rect & r)
+            : vertical(v)
+            , point(p)
+            , rect(r)
+        {
+        }
+        bool vertical = true;
+        const Point point;
+        const Rect rect;
+        std::shared_ptr<Node> left;
+        std::shared_ptr<Node> right;
     };
 
-    PointSet();
+    class iterator
+    {
+    public:
+        using difference_type = std::ptrdiff_t;
+        using value_type = Point;
+        using pointer = value_type *;
+        using reference = value_type &;
+        using iterator_category = std::forward_iterator_tag;
+
+        iterator() = default;
+        iterator(const PointSet & p, std::size_t i)
+            : m_it(i)
+        {
+            m_tree.reserve(p.size());
+            traverse(p.m_root);
+        }
+
+        void traverse(const std::shared_ptr<Node> & node)
+        {
+            if (node == nullptr) {
+                return;
+            }
+            m_tree.push_back(node->point);
+            traverse(node->left);
+            traverse(node->right);
+        }
+
+        reference operator*() { return m_tree[m_it]; }
+        pointer operator->() { return &m_tree[m_it]; }
+        iterator & operator++()
+        {
+            m_it++;
+            return *this;
+        }
+        iterator operator++(int)
+        {
+            auto tmp = *this;
+            operator++();
+            return tmp;
+        }
+        bool operator==(const iterator & other) const
+        {
+            if (m_it != other.m_it // ???????
+                || m_tree.size() != other.m_tree.size()) {
+                return false;
+            }
+            for (size_t i = 0; i < m_tree.size(); i++) {
+                if (m_tree[i] != other.m_tree[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        bool operator!=(const iterator & other) const { return !(operator==(other)); }
+
+    private:
+        int m_it;
+        std::vector<Point> m_tree;
+    };
+
+    PointSet(const std::string & filename = {});
 
     bool empty() const;
     std::size_t size() const;
@@ -90,9 +255,25 @@ public:
 
     std::optional<Point> nearest(const Point &) const;
     std::pair<iterator, iterator> nearest(const Point &, std::size_t) const;
-    std::pair<iterator, iterator> nearest(const Point & p, std::size_t k) const;
 
     friend std::ostream & operator<<(std::ostream &, const PointSet &);
+
+private:
+    static std::function<bool(Point, Point)> pointComparator(const Point & p)
+    {
+        return [&p](Point a, Point b) { return p.distance(a) < p.distance(b) ? -1 : 1; };
+    }
+
+    std::shared_ptr<Node> m_root;
+    std::size_t m_size = 0;
+    mutable std::vector<std::shared_ptr<PointSet>> range_cash;
+    mutable std::vector<std::shared_ptr<PointSet>> nearest_cash;
+
+    bool contains(const std::shared_ptr<Node> & current, const Point & p) const;
+    void put(std::shared_ptr<Node> & current, const Point & p, bool isLeftChild, std::shared_ptr<Node> & parent);
+    void range(const Rect & r, const std::shared_ptr<Node> & node, PointSet & m_range_result) const;
+    void nearest(const Point & p, const Node & current, std::set<Point, decltype(pointComparator(p))> & m_nearest_answer, size_t k) const;
+    bool needToGoLeft(const std::shared_ptr<Node> & current, const Point & p) const;
 };
 
 } // namespace kdtree
--- src/2dtree.cpp
+++ src/2dtree.cpp
@@ -1,1 +1,173 @@
 #include "primitives.h"
+
+namespace kdtree {
+
+using Node = PointSet::Node;
+using iterator = PointSet::iterator;
+using nodePtr = std::shared_ptr<Node>;
+
+PointSet::PointSet(const std::string & filename)
+{
+    std::ifstream in(filename);
+    if (in.is_open()) {
+        double x, y;
+        while (in >> x >> y) {
+            put({x, y});
+        }
+    }
+    in.close();
+}
+
+bool PointSet::empty() const
+{
+    return m_size == 0;
+}
+std::size_t PointSet::size() const
+{
+    return m_size;
+}
+
+void PointSet::put(const Point & p)
+{
+    if (m_root == nullptr) {
+        m_size = 1;
+        m_root = std::make_shared<Node>(p, true, *(new Rect({INT32_MIN, INT32_MIN}, {INT32_MAX, INT32_MAX})));
+        return;
+    }
+    if (!contains(p)) {
+        put(m_root, p, false, m_root);
+    }
+}
+
+bool PointSet::needToGoLeft(const nodePtr & current, const Point & p) const
+{
+    return ((!current->vertical || current == m_root) && p.x() <= current->point.x()) ||
+            ((current->vertical || current == m_root) && p.y() <= current->point.y());
+}
+
+void PointSet::put(nodePtr & current, const Point & p, bool isLeftChild, nodePtr & parent)
+{
+    if (current == nullptr) {
+        m_size++;
+        double xmin = parent->rect.xmin(), xmax = parent->rect.xmax(), ymin = parent->rect.ymin(), ymax = parent->rect.ymax();
+        if (isLeftChild) {
+            if (parent->vertical) {
+                xmax = parent->point.x();
+            }
+            else {
+                ymax = parent->point.y();
+            }
+        }
+        else {
+            if (parent->vertical) {
+                xmin = parent->point.x();
+            }
+            else {
+                ymin = parent->point.y();
+            }
+        }
+        current = std::make_shared<Node>(p, !parent->vertical, *(new Rect({xmin, ymin}, {xmax, ymax})));
+    }
+    else if (needToGoLeft(current, p)) {
+        put(current->left, p, true, current);
+    }
+    else {
+        put(current->right, p, false, current);
+    }
+}
+
+bool PointSet::contains(const nodePtr & current, const Point & p) const
+{
+    if (current == nullptr) {
+        return false;
+    }
+    else if (current->point == p) {
+        return true;
+    }
+    else if (needToGoLeft(current, p)) {
+        return contains(current->left, p);
+    }
+    return contains(current->right, p);
+}
+bool PointSet::contains(const Point & p) const { return !empty() && contains(m_root, p); }
+
+PointSet::iterator PointSet::begin() const { return iterator(*this, 0); }
+PointSet::iterator PointSet::end() const { return iterator(*this, size()); }
+
+void PointSet::range(const Rect & r, const nodePtr & node, PointSet & m_range_result) const
+{
+    if (node == nullptr || !r.intersects(node->rect)) {
+        return;
+    }
+    if (r.contains(node->point)) {
+        m_range_result.put(node->point);
+    }
+    range(r, node->left, m_range_result);
+    range(r, node->right, m_range_result);
+}
+
+std::pair<iterator, iterator> PointSet::range(const Rect & r) const
+{
+    PointSet * m_range_result = new PointSet();
+    range_cash.push_back(std::shared_ptr<PointSet>(m_range_result));
+    range(r, m_root, *m_range_result);
+    return {m_range_result->begin(), m_range_result->end()};
+}
+
+void PointSet::nearest(const Point & p, const Node & current, std::set<Point, decltype(pointComparator(p))> & m_nearest_answer, const size_t k) const
+{
+    if (p.distance(*prev(m_nearest_answer.end())) >= p.distance(current.point) || m_nearest_answer.size() < k) {
+        if (m_nearest_answer.size() == k) {
+            m_nearest_answer.erase(prev(m_nearest_answer.end()));
+        }
+        m_nearest_answer.insert(current.point);
+    }
+
+    if (current.left != nullptr &&
+        ((!current.vertical && p.x() <= current.point.x()) ||
+         (current.vertical && p.y() <= current.point.y()) || current.right == nullptr)) {
+        nearest(p, *current.left, m_nearest_answer, k);
+
+        if (current.right != nullptr && (p.distance(*prev(m_nearest_answer.end())) >= current.right->rect.distance(p) || m_nearest_answer.size() < k)) {
+            nearest(p, *current.right, m_nearest_answer, k);
+        }
+    }
+    else if (current.right != nullptr) {
+        nearest(p, *current.right, m_nearest_answer, k);
+
+        if (current.left != nullptr && (p.distance(*prev(m_nearest_answer.end())) >= current.left->rect.distance(p) || m_nearest_answer.size() < k)) {
+            nearest(p, *current.left, m_nearest_answer, k);
+        }
+    }
+}
+
+std::optional<Point> PointSet::nearest(const Point & p) const
+{
+    std::set<Point, decltype(pointComparator(p))> m_nearest_answer(pointComparator(p));
+    m_nearest_answer.insert(m_root->point);
+    nearest(p, *m_root, m_nearest_answer, 1);
+    return *m_nearest_answer.begin();
+}
+std::pair<iterator, iterator> PointSet::nearest(const Point & p, std::size_t k) const
+{
+    PointSet ans;
+    if (k == 0) {
+        return {ans.begin(), ans.end()};
+    }
+    std::set<Point, decltype(pointComparator(p))> m_nearest_answer(pointComparator(p));
+    m_nearest_answer.insert(m_root->point);
+    nearest(p, *m_root, m_nearest_answer, k);
+    for (auto i : m_nearest_answer) {
+        ans.put(i);
+    }
+    return {ans.begin(), ans.end()};
+}
+
+std::ostream & operator<<(std::ostream & out, const PointSet & p)
+{
+    for (auto it : p) {
+        out << &it << std::endl;
+    }
+    return out;
+}
+} // namespace kdtree
